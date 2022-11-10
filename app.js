8eef4c650546cefb696a600860b7ae97c1ff881e
sivashankar97 <hexshankar1997@gmail.com>
2022-11-10 19:35:31 +0530

    functions

--- app.js
+++ app.js
@@ -215,3 +215,27 @@ for (let i = 0; i < gameBoard.length; i++) {
     totalScore += row[j];
   }
 }
+
+
+function greet(nickname) {
+  console.log(`Hi, ${nickname}!`);
+}
+greet("Sansa");
+greet("Ramsay");
+
+// EXAMPLE 2
+function rollDie() {
+  let roll = Math.floor(Math.random() * 6) + 1;
+  console.log(`Rolled: ${roll}`);
+}
+// We can now specify how many dice to roll!
+function throwDice(numRolls) {
+  for (let i = 0; i < numRolls; i++) {
+    rollDie();
+  }
+}
+
+throwDice(2);
+throwDice(6);
+
+
