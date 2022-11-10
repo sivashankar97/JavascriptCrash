864a664cc97cb8be3a703aeae6284d950b1b5869
sivashankar97 <hexshankar1997@gmail.com>
2022-11-10 22:07:55 +0530

    chikidor created

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log("hi");
+const chokidar = require("chokidar");
+
+chokidar
+  .watch(".")
+  .on("add", () => console.log("file added"))
+  .on("change", () => console.log("file changed"))
+  .on("unlink", () => console.log("file unlinked"));
