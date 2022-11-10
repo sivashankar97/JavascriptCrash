4743c13e07103eca3a50fe7a8c3e368e869ae3c9
sivashankar97 <hexshankar1997@gmail.com>
2022-11-10 22:36:49 +0530

    caporal insatalled

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const chokidar = require("chokidar");
+const debounce = require("lodash.debounce");
+const prog = require("caporal");
+
+prog
+  .version("0.01")
+  .argument("[filename]", "name of a file to execute")
+  .action((args) => {
+    console.log(args);
+  });
+
+  prog.parse(process.argv);
+
+const start = debounce(() => {
+  console.log("Starting User programe");
+}, 100);
 
 chokidar
   .watch(".")
-  .on("add", () => console.log("file added"))
+  .on("add", start)
   .on("change", () => console.log("file changed"))
   .on("unlink", () => console.log("file unlinked"));
