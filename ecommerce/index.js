795886bf97a69545cc03da93c2927872dc0a548e
sivashankar97 <hexshankar1997@gmail.com>
2022-11-11 11:43:28 +0530

    middleware added

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -1,20 +1,44 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send(`
-  <div>
-  <form method="POST" >
-   <input placeholder="email"/>
-   <input placeholder="password" />
-   <input placeholder="password confrimation" />
-   <button> Sign Up </button>
-  </form>
-  </div>
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
   `);
 });
 
+// const bodyParser = (req, res, next) => {
+//   if (req.method === "POST") {
+//     req.on("data", (data) => {
+//       const parsed = data.toString("utf8").split("&");
+//       const formData = {};
+//       for (let pair of parsed) {
+//         const [key, value] = pair.split("=");
+//         formData[key] = value;
+//       }
+//       req.body = formData;
+//       next();
+//     });
+//   } else {
+//     next();
+//   }
+// };
+
+app.post("/", bodyParser.urlencoded({ extended: true }), (req, res) => {
+  console.log(req.body);
+  res.send("Account created!!!");
+});
+
 app.listen(3000, () => {
-  console.log("listening");
+  console.log("Listening");
 });
