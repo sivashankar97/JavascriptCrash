28e4361a4db5b2bd01f31192219697ae5bc32cde
sivashankar97 <hexshankar1997@gmail.com>
2022-11-11 14:23:15 +0530

    create method created

--- ecommerce/repositories/users.js
+++ ecommerce/repositories/users.js
@@ -12,9 +12,46 @@ class UsersRepository {
       fs.writeFileSync(this.filename, "[]");
     }
   }
-  async checkForFile() {}
+
+  //method1
+  //   async getAll() {
+  //     const contents = await fs.promises.readFile(this.filename, {
+  //       encoding: "utf8",
+  //     });
+  //     console.log(contents);
+
+  //     const data = JSON.parse(contents);
+
+  //     return data;
+  //   }
+  // }
+
+  //method2
+  //getting method
+  async getAll() {
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: "utf8",
+      })
+    );
+  }
+
+  async create(attrs) {
+    const records = await this.getAll();
+    records.push(attrs);
+
+    await fs.promises.writeFile(this.filename, JSON.stringify(records));
+  }
 }
 
-const repo = new UsersRepository("users.json");
+const test = async () => {
+  const repo = new UsersRepository("users.json");
 
+  await repo.create({ email: "testddd@test.com", password: "password" });
+
+  const users = await repo.getAll();
+
+  console.log(users);
+};
 //creating data store file
+test();
