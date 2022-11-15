5c518dfcbbf908853ef2544bfc8607081793c413
sivashankar97 <hexshankar1997@gmail.com>
2022-11-15 12:49:25 +0530

    saving file

--- ecommerce/routes/admin/product.js
+++ ecommerce/routes/admin/product.js
@@ -3,6 +3,7 @@ const { validationResult } = require("express-validator");
 // const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/newone");
 const { requireTitle, requirePrice } = require("./validator");
+const productsRepo = require("../../repositories/product");
 
 const router = express.Router();
 
@@ -12,12 +13,16 @@ router.get("/admin/products/new", (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-  const errors = validationResult(req);
-
-  console.log(req.body);
-
-  res.send("submitted");
-});
+router.post(
+  "/admin/products/new",
+  [requireTitle, requirePrice],
+  upload.single("image"),
+  async (req, res) => {
+    const image = req.file.buffer.toString("base64");
+    const { title, price } = req.body;
+    await productsRepo.create({ title, price, image });
+    res.send("submitted");
+  }
+);
 
 module.exports = router;
