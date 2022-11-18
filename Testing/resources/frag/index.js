b0a2b37f62acef13e75d85b8b2d82fc685ff89db
sivashankar97 <hexshankar1997@gmail.com>
2022-11-18 21:18:39 +0530

    small issues checkd and fixed

--- Testing/resources/frag/index.js
+++ Testing/resources/frag/index.js
@@ -1,11 +1,8 @@
-#!/usr/bin/env node
-
-const Runner = require('./runner');
+const Runner = require("./runner");
 const runner = new Runner();
 
 const run = async () => {
   await runner.collectFiles(process.cwd());
   runner.runTests();
 };
-
 run();
--- Testing/resources/frag/samplewebproject/index.js
+++ Testing/resources/frag/samplewebproject/index.js
@@ -1,12 +1,10 @@
-document.querySelector('form').addEventListener('submit', event => {
+document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const { value } = document.querySelector('input');
-
-  const header = document.querySelector('h1');
-  if (value.includes('@')) {
-    header.innerHTML = 'Looks good!';
+  const { value } = document.querySelector("input");
+  const header = document.querySelector("h1");
+  if (value.includes("@")) {
+    header.innerHTML = "Looks good!";
   } else {
-    header.innerHTML = 'Invalid email';
+    header.innerHTML = "Invalid email";
   }
 });
