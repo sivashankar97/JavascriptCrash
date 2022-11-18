ae84467c123cca7ab86908dd34179a6893901268
sivashankar97 <hexshankar1997@gmail.com>
2022-11-18 18:21:27 +0530

    totaling cart items

--- ecommerce/routes/products.js
+++ ecommerce/routes/products.js
@@ -3,7 +3,6 @@ const productsRepo = require("../repositories/products");
 const productsIndexTemplate = require("../views/products/index");
 
 const router = express.Router();
-
 router.get("/", async (req, res) => {
   const products = await productsRepo.getAll();
   res.send(productsIndexTemplate({ products }));
